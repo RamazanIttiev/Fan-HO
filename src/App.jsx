c981591b82f8a68cc8c5297c0691d24513d90ae5
Ramazan <vip.ittiev@mail.ru>
2020-05-31 17:43:38 +0300

    Adapted mark up
    Laptops/tablets/mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import "./App.css";
-import "./Scss/Blocks.scss";
-import "./Scss/Bootstrap-reboot.min.scss";
+import React from 'react'
+import './App.css'
+import './Scss/Blocks.scss'
 
-import Promo from "./Promo/Promo.jsx";
-import Bio from "./Bio/Bio.jsx";
-import Works from "./Works/Works.jsx";
-import Books from "./Books/Books.jsx";
-import Footer from "./Footer/Footer.jsx";
+import Promo from './Promo/Promo.jsx'
+import Bio from './Bio/Bio.jsx'
+import Works from './Works/Works.jsx'
+import Books from './Books/Books.jsx'
+import Footer from './Footer/Footer.jsx'
+
+import './Scss/Bootstrap-reboot.min.scss'
+import './Scss/Media.scss'
 
 function App() {
   return (
@@ -28,7 +30,7 @@ function App() {
         <Footer />
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
